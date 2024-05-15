0a85f778cf4656b033ad84b57aef6bd97c4a4092
Starchaser1993 <sebastiantorresmattisen@gmail.com>
2024-05-15 18:38:11 -0400

    Actualización 15-05-2024

--- hello.py
+++ hello.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, url_for
 
 app = Flask(__name__)
 
-# Ruta para la página de inicio
+# Cargar datos del archivo JSON
+with open('data.json') as f:
+    data = json.load(f)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Ruta para la vista de publicación
-@app.route('/post_view')
-def post_view():
-    return render_template('post_view.html')
+@app.route('/view_data')
+def view_data():
+    return render_template('view_data.html', data=data)
+
+@app.route('/detail/<int:item_id>')
+def detail(item_id):
+    item = next((item for item in data if item['id'] == item_id), None)
+    return render_template('detail.html', item=item)
 
 if __name__ == '__main__':
     app.run(debug=True)
